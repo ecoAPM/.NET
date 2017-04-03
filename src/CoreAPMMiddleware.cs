356dda7666394676a1eb64df4e902ae6e849757f
Steve Desmond <steve@vtsv.ca>
2017-04-03 09:52:12 -0400

    initial code commit

---
+++ src/CoreAPMMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreAPM.DotNet.Agent;
+using CoreAPM.Events.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace CoreAPM.DotNet.ASPNETCoreMiddleware
+{
+    public class CoreAPMMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IAgent _agent;
+        private readonly ITimer _timer;
+
+        public CoreAPMMiddleware(RequestDelegate next, IAgent agent, ITimer timer)
+        {
+            _timer = timer;
+            _next = next;
+            _agent = agent;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var time = getRequestTime(httpContext);
+            var e = new Event
+            {
+                ID = Guid.NewGuid(),
+                Type = "ServerResponse",
+                Source = httpContext.Request.Host.Value,
+                Action = httpContext.Request.Path.Value,
+                Result = httpContext.Response.StatusCode.ToString(),
+                Time = DateTime.Now,
+                Length = await time
+            };
+            _agent.Send(e);
+        }
+
+        private async Task<double> getRequestTime(HttpContext httpContext)
+        {
+            _timer.Start();
+            await _next.Invoke(httpContext);
+            _timer.Stop();
+            return _timer.CurrentTime;
+        }
+    }
+}
---
+++ src/CoreAPMMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CoreAPM.DotNet.Agent;
+using Microsoft.AspNetCore.Builder;
+
+namespace CoreAPM.DotNet.ASPNETCoreMiddleware
+{
+    public static class CoreAPMMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCoreAPM(this IApplicationBuilder app) => app.UseMiddleware<CoreAPMMiddleware>();
+        public static IApplicationBuilder UseCoreAPM(this IApplicationBuilder app, Config config) => app.UseMiddleware<CoreAPMMiddleware>(config);
+    }
+}
