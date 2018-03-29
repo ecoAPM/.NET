8fbdf36c82ba0ce6757f84af7138fe2ce40dd27d
Steve Desmond <steve@vtsv.ca>
2018-03-29 11:09:51 -0400

    add manual test app

---
+++ CoreAPM.NET.TestApp/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAPM.DotNet.TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "Hello world!";
+    }
+}
