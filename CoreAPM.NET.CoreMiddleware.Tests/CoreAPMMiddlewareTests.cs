7e8dd3cebd50faf1bf1e876de226e5a218cab500
Steve Desmond <steve@vtsv.ca>
2018-05-10 20:20:41 -0400

    middleware returns time in UTC

--- CoreAPM.NET.CoreMiddleware.Tests/CoreAPMMiddlewareTests.cs
+++ CoreAPM.NET.CoreMiddleware.Tests/CoreAPMMiddlewareTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CoreAPM.NET.Agent;
 using Microsoft.AspNetCore.Http;
@@ -44,23 +45,43 @@ namespace CoreAPM.NET.CoreMiddleware.Tests
         }
 
         [Fact]
-        public async Task ReportsTimeFromTimer()
+        public async Task ReportsLengthFromTimer()
         {
             //arrange
             var next = Substitute.For<RequestDelegate>();
             var timer = Substitute.For<ITimer>();
             var agent = Substitute.For<IAgent>();
             var context = Substitute.For<HttpContext>();
-            var time = 0.0;
+            var length = 0.0;
             timer.CurrentTime.Returns(123);
-            agent.When(a => a.Send(Arg.Any<Event>())).Do(c => time = ((Event)c.Args().First()).Length);
+            agent.When(a => a.Send(Arg.Any<Event>())).Do(c => length = ((Event)c.Args().First()).Length);
             var middleware = new CoreAPMMiddleware(next, agent, () => timer);
 
             //act
             await middleware.Invoke(context);
 
             //assert
-            Assert.Equal(123, time);
+            Assert.Equal(123, length);
+        }
+
+        [Fact]
+        public async Task ReportsTimeInUTC()
+        {
+            //arrange
+            var next = Substitute.For<RequestDelegate>();
+            var timer = Substitute.For<ITimer>();
+            var agent = Substitute.For<IAgent>();
+            var context = Substitute.For<HttpContext>();
+            var time = new DateTime();
+            timer.CurrentTime.Returns(123);
+            agent.When(a => a.Send(Arg.Any<Event>())).Do(c => time = ((Event)c.Args().First()).Time);
+            var middleware = new CoreAPMMiddleware(next, agent, () => timer);
+
+            //act
+            await middleware.Invoke(context);
+
+            //assert
+            Assert.Equal(time.ToUniversalTime(), time);
         }
     }
 }
